b450d0a3b032305582f99948fea81ce427a5611a
Pedro Bacchini <pedrobacchini@outlook.com>
2019-07-01 16:51:41 -0300

    First Commit

---
+++ src/main/java/com/github/pedrobacchini/introspringdynamodb/IntroSpringDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.introspringdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringDynamodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroSpringDynamodbApplication.class, args);
+    }
+
+}
